30944957255e93ee577e9c943f189d6692c8dfe4
Chua Hou <human+github@chuahou.dev>
2020-06-15 11:40:15 +0800

    xkcd37: improve handling of hyphens

--- xkcd37/Xkcd37.hs
+++ xkcd37/Xkcd37.hs
@@ -4,22 +4,21 @@
 xkcdify :: String -> String
 xkcdify = unwords . assify . words
 
-assEnd :: String
-assEnd = "-ass"
-
-assEnd' :: String
-assEnd' = tail assEnd
-
-assPre :: String
-assPre = cs ++ [c]
-    where (c:cs) = assEnd
-
 assify :: [String] -> [String]
 assify []   = []
 assify [cs] = [cs]
 assify (cs:ds:css)
-    | endcs == assEnd   = startcs  : (assPre ++ ds) : assify css
-    | endcs' == assEnd' = startcs' : (assPre ++ ds) : assify css
-    | otherwise       = cs : assify (ds:css)
-        where (startcs,  endcs)  = splitAt (length cs - length assEnd) cs
-              (startcs', endcs') = (startcs ++ [head endcs], tail endcs)
+    | endcs == ass = startcs' : (ass ++ (hyphen:ds)) : assify css
+    | otherwise    = cs       : assify (ds:css)
+        where (startcs, endcs) = splitAt (length cs - length ass) cs
+              startcs' = if last startcs == hyphen -- if ends in hyphen
+                         then init startcs         -- discard that hyphen
+                         else startcs
+
+----- Constants -----
+
+ass :: String
+ass = "ass"
+
+hyphen :: Char
+hyphen = '-'
