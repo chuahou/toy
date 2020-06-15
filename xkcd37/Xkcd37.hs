2a058947148bdbba750072e085bcec3302df10a6
Chua Hou <human+github@chuahou.dev>
2020-06-15 11:45:14 +0800

    xkcd37: fix more edge cases (empty startcs)

--- xkcd37/Xkcd37.hs
+++ xkcd37/Xkcd37.hs
@@ -2,18 +2,19 @@
 -- Copyright (c) 2020 Chua Hou
 
 xkcdify :: String -> String
-xkcdify = unwords . assify . words
+xkcdify = unwords . filter (not . null) . assify . words
 
 assify :: [String] -> [String]
 assify []   = []
 assify [cs] = [cs]
 assify (cs:ds:css)
-    | endcs == ass = startcs' : (ass ++ (hyphen:ds)) : assify css
-    | otherwise    = cs       : assify (ds:css)
+    | endcs == ass = prunehyp startcs : (ass ++ (hyphen:ds)) : assify css
+    | otherwise    = cs               : assify (ds:css)
         where (startcs, endcs) = splitAt (length cs - length ass) cs
-              startcs' = if last startcs == hyphen -- if ends in hyphen
-                         then init startcs         -- discard that hyphen
-                         else startcs
+              prunehyp ""     = ""
+              prunehyp (c:"") | c == hyphen = ""
+                              | otherwise   = [c]
+              prunehyp (c:cs) = c:prunehyp cs
 
 ----- Constants -----
 
